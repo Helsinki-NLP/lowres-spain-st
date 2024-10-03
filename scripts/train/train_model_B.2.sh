7600fb20300bcbc6f2a40b83dc1a7610d50ca122
Ona de Gibert <odegiber@gmail.com>
2024-10-03 12:01:14 +0300

    Added training scripts for models A and B

---
+++ scripts/train/train_model_B.2.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH --job-name="finetune_spa-itc"
+#SBATCH -o logs/finetune_spa-itc_%j.out
+#SBATCH -e logs/finetune_spa-itc_%j.err
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:v100:4
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=32G
+#SBATCH --account=project_2001194
+#SBATCH --time=72:00:00
+
+# Substitute >>oci<< for >>xxx<< for Aranese
+#sed "s/^>>oci<< />>xxx<< /" data/train/train.spa-oci.spa.tok > data/train/train.spa-oci.spa.xxx.tok
+
+# Concat all train data with xxx tag for Aranese
+#cat data/train/train.spa-arg.spa.tok data/train/train.spa-ast.spa.tok data/train/train.spa-oci.spa.xxx.tok data/train/train.spa-ara.spa.tok > data/train/train.src.xxx.oci.tok
+
+# OPUS+bibles model
+opusmodeldir="models/OPUS-MT+bibles/deu+eng+fra+por+spa-itc/"
+opusmodelname="opusTCv20230926max50+bt+jhubc.spm32k-spm32k.transformer-big.model1.npz.best-perplexity.npz"
+vocab="opusTCv20230926max50+bt+jhubc.spm32k-spm32k.vocab.yml"
+
+# Output model
+modeldir="models/opus+bibles_ft_all_xxx_2_bleu"
+datadir="data/train"
+
+mkdir -p $modeldir
+mkdir -p ${modeldir}/valid_logs
+
+# Marian
+MARIAN="/scratch/project_2005815/degibert/marian-dev/build"
+
+# Fine-tune
+${MARIAN}/marian --model "${modeldir}/model.npz" \
+       --pretrained-model "${opusmodeldir}/${opusmodelname}" \
+       -v "${opusmodeldir}/$vocab" "${opusmodeldir}/$vocab" \
+       --valid-sets "${datadir}/dev.src.tok" "${datadir}/dev.trg.tok"  \
+       --train-sets "${datadir}/train.src.xxx.oci.tok" "${datadir}/train.trg.tok" \
+       --valid-translation-output "${modeldir}/valid_logs/validation-output-after-{U}-updates-{E}-epochs.txt" \
+       --log "${modeldir}/train.log" --valid-log "${modeldir}/valid.log" \
+       --task transformer-big \
+       --optimizer-delay 2 \
+       --early-stopping 10 \
+       --valid-freq 2500 \
+       --valid-metrics bleu perplexity ce-mean-words \
+       --valid-mini-batch 16 \
+       --beam-size 6 \
+       --normalize 1 \
+       --allow-unk \
+       --workspace 25000 \
+       --save-freq 2500 \
+       --disp-freq 2500 \
+       --devices 0 1 2 3 \
+       --seed 1111 \
+       --shuffle batches \
+       --sharding local \
+       --disp-first 10 \
+       --disp-label-counts \
+       --keep-best
